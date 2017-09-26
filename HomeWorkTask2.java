5fc421231cc85da024322feef298570a2305f067
SmirnovNikita <32300621+SmirnovNikita@users.noreply.github.com>
2017-09-26 16:52:07 +0300

    Update HomeWorkTask2.java

--- HomeWorkTask2.java
+++ HomeWorkTask2.java
@@ -4,19 +4,19 @@ public class HomeWorkTask2 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		double sideA;
-		System.out.println("Введите сторону треугольника А");
-			sideA=sc.nextDouble();
-	double sideB;
-		System.out.println("Введите сторону треугольника B");
-			sideB=sc.nextDouble();
-	double sideC;
-		System.out.println("Введите сторону трегольника C");
-			sideC=sc.nextDouble();
-	double semiPerimeter=(sideA+sideB+sideC)/2;
-	double area=Math.sqrt(semiPerimeter*(semiPerimeter-sideA)*(semiPerimeter-sideB)*(semiPerimeter-sideC));
-	System.out.println("Полупериметр треугольника="+semiPerimeter);
-	System.out.println("Площадь треугольника="+area);
-	sc.close();
+		System.out.println("Input side triangle A");
+		sideA=sc.nextDouble();
+		double sideB;
+		System.out.println("Input side triangle B");
+		sideB=sc.nextDouble();
+		double sideC;
+		System.out.println("Input side triangle C");
+		sideC=sc.nextDouble();
+		double semiPerimeter=(sideA+sideB+sideC)/2;
+		double area=Math.sqrt(semiPerimeter*(semiPerimeter-sideA)*(semiPerimeter-sideB)*(semiPerimeter-sideC));
+		System.out.println("Half perimeter triangle="+semiPerimeter);
+		System.out.println("Area triangle="+area);
+		sc.close();
 	}
 
 }
